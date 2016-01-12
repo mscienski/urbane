b27482143ec2d1ffbbf791eb2511b2f91d90b8a7
mscienski <mscienski@gmail.com>
2016-01-11 21:15:07 -0700

    Add boilerplate

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:saiarcot895/myppa
-sudo apt-get update
-sudo apt-get -y install apt-fast
+sudo apt-get remove nodejs
 
-curl --silent --location https://deb.nodesource.com/setup_4.2.2 | sudo -E bash -
+curl --silent --location https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-add-repository ppa:brightbox/ruby-ng
-sudo chown -R `whoami` /etc/apt/sources.list.d/
-sudo apt-fast update
-sudo apt-fast install -y ruby2.2 ruby2.2-dev git nodejs
+sudo apt-get update
+sudo apt-get install -y ruby2.2 ruby2.2-dev git nodejs
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
