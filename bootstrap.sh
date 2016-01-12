ce947226dd4ba984449d599d9188c5e86fdb75e5
mscienski <mscienski@gmail.com>
2016-01-11 21:37:57 -0700

    fix vagrant provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt-get remove nodejs
+sudo apt-get remove -y nodejs
 
 curl --silent --location https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
 sudo apt-get install -y ruby2.2 ruby2.2-dev git nodejs
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+sudo cp /vagrant ~/stpauls -r
+cd ~/stpauls
+sudo chown -R `whoami` ./
+sudo npm install -g npm@"~3.3.10"
+npm install
